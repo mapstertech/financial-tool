927ebb3356b24318560cd2358023ea14469901ea
Keano <keanov@gmail.com>
2018-07-03 14:07:06 -0700

    Trying puppeteer

--- index.js
+++ index.js
@@ -1,62 +1,26 @@
-const axios = require('axios');
-const cheerio = require('cheerio')
+var page = require('webpage').create();
 
-const BASE_TMX_URL = 'https://web.tmxmoney.com/financials.php'
+var BASE_TMX_URL = 'https://web.tmxmoney.com/financials.php';
+var tickerSymbol = 'RY'
+var url = BASE_TMX_URL + '?qm_symbol=' + tickerSymbol;
 
-async function getCompanyData(tickerSymbol) {
-    const url = `${BASE_TMX_URL}?qm_symbol=${tickerSymbol}`
-    console.log({ url })
-
-    let html, json;
-    let data = [];
-
-    try {
-        // const res = await axios.get(BASE_TMX_URL + '?qm_symbol=' + tickerSymbol);
-        const res = await axios.get(url);
-        html = res.data;
-        // console.log(html)
-    } catch (err) {
-        console.log(err.message);
-    }
-
-    const $ = cheerio.load(html);
-
-    console.log(html)
-
-
-
-    let table_rows = $('.qmod-rowtitle');
-    console.log('table_rows', table_rows)
-
-
-    table_rows.each(function(i, row) {
-
-
-
-        // data.push({
-    	// 	title: $(this).children[0].innerText,
-    	// 	Y1: row.children[2].innerText,
-    	// 	Y2: row.children[3].innerText,
-    	// 	Y3: row.children[4].innerText,
-    	// 	Y4: row.children[5].innerText,
-    	// 	Y5: row.children[6].innerText,
-    	// })
-    });
-
-    console.log('data', data);
-
-    return data;
+page.onConsoleMessage = function(msg) {
+    console.log(msg);
 }
 
-getCompanyData('RY').then(data => {console.log(data)})
+console.log('Opening page at:', url)
+page.open(url, function(status) {
+    console.log("Status: " + status);
+    if (status !== "success") {
+        console.log('Error opening the page')
+    } else  {
+        var table_data = page.evaluate(function() {
+            return document.querySelectorAll('.qmod-rowtitle');
+        });
 
-// table_rows.forEach((row) => {
-// 	console.log({
-// 		title: row.children[0].innerText,
-// 		Y1: row.children[2].innerText,
-// 		Y2: row.children[3].innerText,
-// 		Y3: row.children[4].innerText,
-// 		Y4: row.children[5].innerText,
-// 		Y5: row.children[6].innerText,
-// 	})
-// });
+        for (var i = 0; i < table_data.length; i++) {
+            console.log(table_data[i].children[0])
+        }
+    }
+    phantom.exit();
+});
